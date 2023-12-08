f92d8a4c171b839a1d6ca926d1525a80f360daa6
Aaron Franke <aaron.franke13@gmail.com>
2023-12-08 00:01:40 -0700

    Add recipe template and other needed java files.

--- src/main/java/co/finecode/controller/RecipeController.java
+++ src/main/java/co/finecode/controller/RecipeController.java
@@ -1,4 +1,22 @@
 package co.finecode.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.awt.print.Book;
+
+@RestController
+@RequestMapping("api")
 public class RecipeController {
+
+    @GetMapping("/{id}")
+    public String getId(@PathVariable int id) {
+        return "Id is " + id;
+    }
+
+    @GetMapping("/recipe")
+    ModelAndView recipe() {
+        return new ModelAndView("recipe");
+    }
 }
---
+++ src/main/java/co/finecode/dto/inventory/FoodItem.java
@@ -0,0 +1,79 @@
+package co.finecode.dto.inventory;
+
+import java.util.Date;
+
+public class FoodItem {
+
+    private int id;
+    private int categoryId;
+    private int subCategoryId;
+    private int vendorId;
+    private String name;
+    private float amount;
+    private String description;
+    private Date createDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public int getSubCategoryId() {
+        return subCategoryId;
+    }
+
+    public void setSubCategoryId(int subCategoryId) {
+        this.subCategoryId = subCategoryId;
+    }
+
+    public int getVendorId() {
+        return vendorId;
+    }
+
+    public void setVendorId(int vendorId) {
+        this.vendorId = vendorId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+}
